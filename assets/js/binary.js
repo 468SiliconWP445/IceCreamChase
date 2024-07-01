a9e7fbb7e1e189f620ba2c05915b932d58664b59
468SiliconWP445 <hotlap8878@gmail.com>
2024-07-01 18:31:56 +0100

    1.0.1

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.53/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"1.0.0","target":"arcade","targetVersion":"1.12.53","repo":"468siliconwp445/icecreamchase"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.53/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"1.0.1","target":"arcade","targetVersion":"1.12.53","repo":"468siliconwp445/icecreamchase"}
 // total=1704583 new=4.90% cached=89.98% other=5.12%
 (function (ectx) {
 'use strict';
@@ -530,7 +530,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P68113(s) {
+function _main___P68124(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -1043,13 +1043,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P68113.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P68113.continuations = [  ]
+_main___P68124.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P68124.continuations = [  ]
 
-function _main___P68113_mk(s) {
+function _main___P68124_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P68113, depth: s.depth + 1,
+        parent: s, fn: _main___P68124, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -57938,5 +57938,5 @@ const sprites_BaseSpriteSayRenderer__C47668_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, ["mySprite___49740","mySprite2___49741"])
 
-return _main___P68113
+return _main___P68124
 })
