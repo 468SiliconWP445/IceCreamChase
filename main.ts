2475f6774dbe0d9df33a50894bbd503374dd6aad
468SiliconWP445 <hotlap8878@gmail.com>
2024-07-01 18:57:08 +0100

    Update (Score: 3)

--- main.ts
+++ main.ts
@@ -15,6 +15,12 @@ mySprite3.setStayInScreen(true)
 mySprite3.setBounceOnWall(true)
 mySprite3.setPosition(30, 20)
 mySprite3.setVelocity(50, 50)
+let mySprite4 = sprites.create(assets.image`idasset-icecream`, SpriteKind.Food)
+mySprite4.setScale(0.425, ScaleAnchor.Middle)
+mySprite4.setStayInScreen(true)
+mySprite4.setBounceOnWall(true)
+mySprite4.setPosition(51, 75)
+mySprite4.setVelocity(50, 50)
 game.onUpdate(function () {
     if (mySprite.overlapsWith(mySprite2)) {
         sprites.destroy(mySprite2)
@@ -22,15 +28,27 @@ game.onUpdate(function () {
     }
 })
 game.onUpdate(function () {
-    if (info.score() == 2) {
+    if (mySprite.overlapsWith(mySprite3)) {
+        sprites.destroy(mySprite3)
+        info.changeScoreBy(1)
+    }
+})
+game.onUpdate(function () {
+    if (info.score() == 3) {
         game.gameOver(true)
         game.setGameOverEffect(true, effects.confetti)
         game.setGameOverMessage(true, "You Won!")
     }
 })
 game.onUpdate(function () {
-    if (mySprite.overlapsWith(mySprite3)) {
-        sprites.destroy(mySprite3)
+    if (mySprite.overlapsWith(mySprite4)) {
+        sprites.destroy(mySprite4)
         info.changeScoreBy(1)
+        mySprite4 = sprites.create(assets.image`idasset-icecream`, SpriteKind.Food)
+        mySprite4.setScale(0.425, ScaleAnchor.Middle)
+        mySprite4.setStayInScreen(true)
+        mySprite4.setBounceOnWall(true)
+        mySprite4.setPosition(51, 75)
+        mySprite4.setVelocity(50, 50)
     }
 })
