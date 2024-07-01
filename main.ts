73e6e1c0a6c2cb61aa67df7f7aecca5d877b11d0
468SiliconWP445 <hotlap8878@gmail.com>
2024-07-01 19:00:42 +0100

    (Score: 100)

--- main.ts
+++ main.ts
@@ -34,7 +34,7 @@ game.onUpdate(function () {
     }
 })
 game.onUpdate(function () {
-    if (info.score() == 50) {
+    if (info.score() == 100) {
         game.gameOver(true)
         game.setGameOverEffect(true, effects.confetti)
         game.setGameOverMessage(true, "You Won!")
